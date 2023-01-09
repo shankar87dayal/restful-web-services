2c0135de5e04630fd1212019c7229b1764d1ee8d
shankar87dayal <shankar.87.dayal@gmail.com>
2023-01-09 06:42:41 +0530

    create spring boot project and test helloworld class and test on browser.

---
+++ src/main/java/com/learn/m_s/rest/webservice/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.learn.m_s.rest.webservice.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+
+    //URL- hello-world
+
+    //Return - Hello_world
+
+    //    @RequestMapping(method = RequestMethod.GET,path ="/hello-world" )
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello_world";
+    }
+
+}

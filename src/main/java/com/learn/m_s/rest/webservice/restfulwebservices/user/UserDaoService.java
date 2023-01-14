71767d0fd0e45bbcf946efd56960f27a27075353
shankar87dayal <shankar.87.dayal@gmail.com>
2023-01-14 08:50:56 +0530

    Implementing Generic Exception Handling for all Resources

--- src/main/java/com/learn/m_s/rest/webservice/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/learn/m_s/rest/webservice/restfulwebservices/user/UserDaoService.java
@@ -7,25 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import org.springframework.stereotype.Component;
+
 @Component
 public class UserDaoService {
-
-    //JPA/Hibernate  > Database
-    //UserDaoService > Static List
+    // JPA/Hibernate > Database
+    // UserDaoService > Static List
 
     private static List<User> users = new ArrayList<>();
+
     private static int usersCount = 0;
 
     static {
-        users.add(new User(++usersCount, "shivam", LocalDate.now().minusYears(20)));
-        users.add(new User(++usersCount, "Hari om", LocalDate.now().minusYears(25)));
-        users.add(new User(++usersCount, "ajay", LocalDate.now().minusYears(18)));
+        users.add(new User(++usersCount,"Shivam",LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"Ravi",LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"Hari om",LocalDate.now().minusYears(20)));
     }
 
-    public  List<User> findAll() {
+    public List<User> findAll() {
         return users;
     }
-    public User save (User user){
+
+    public User save(User user) {
         user.setId(++usersCount);
         users.add(user);
         return user;
@@ -35,4 +38,11 @@ public class UserDaoService {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().orElse(null);
     }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
+
+
 }

0dd7ecadd9f4d3e0e1db374586c9837785d4a824
shankar87dayal <shankar.87.dayal@gmail.com>
2023-01-17 07:40:40 +0530

    Implementing Versioning REST API - Request Param, Header and Content Negotiation

--- src/main/java/com/learn/m_s/rest/webservice/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/learn/m_s/rest/webservice/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,33 @@ public class VersioningPersonController {
     public PersionV2 getSecondVersionOfPerson(){
         return new PersionV2(new Name("Raushan", "Ranjan"));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersionV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersionV1("Raushan Ranjan");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersionV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersionV2(new Name("Raushan", "Ranjan"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersionV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersionV1("Raushan Ranjan");
+    }
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersionV2 getSecondVersionOfPersonRequestHeader(){
+        return new PersionV2(new Name("Raushan", "Ranjan"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersionV1 getFirstVersionOfPersonAcceptHeader(){
+        return new PersionV1("Raushan Ranjan");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersionV2 getSecondVersionOfPersonAcceptHeader(){
+        return new PersionV2(new Name("Raushan", "Ranjan"));
+    }
 }

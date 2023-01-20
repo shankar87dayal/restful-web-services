a9b616c73336c74db9c803c5c9485669c0cf9311
shankar87dayal <shankar.87.dayal@gmail.com>
2023-01-20 07:15:18 +0530

    we created the post entity,we established the relationship with the user details,and we also populated some data into the post entity.Creating Post Entity with Many to One Relationship with User Entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ values(1005, current_date(),'Sumit');
 
 insert into user_details(id,birth_date,name)
 values(1009, current_date(),'Rahul');
+
+insert into post(id,description,user_id)
+values(2001,'i want to learn AWS',1001);
+
+insert into post(id,description,user_id)
+values(2002,'i want to learn DevOps',1001);
+
+insert into post(id,description,user_id)
+values(2003,'i want to Get AWS Certified',1002);
+
+insert into post(id,description,user_id)
+values(2004,'i want to learn Multi Cloud',1002);
